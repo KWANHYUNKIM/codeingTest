837674fb8aa1793186a2014aabae4a2217f478a7
kwanhyunkim <55569899+KWANHYUNKIM@users.noreply.github.com>
2025-02-19 20:22:13 +0900

    20250219

---
+++ 그래프이론/p20364.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class p20364{
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input[] = br.readLine().split(" ");
+
+        int N = Integer.parseInt(input[0]);
+        int Q = Integer.parseInt(input[1]);
+
+        
+    }
+}
