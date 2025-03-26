6e8ea34dce507a955fe73d242c56df15bb5441ec
kwanhyunkim <55569899+KWANHYUNKIM@users.noreply.github.com>
2025-03-26 13:27:09 +0900

    20250326

--- 구현/p8949.java
+++ 구현/p8949.java
@@ -5,22 +5,39 @@ public class p8949{
     public static void main(String args[]) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        String A = br.readLine();
-        String B = br.readLine();
+        String input [] = br.readLine().split(" ");
+        String A = input[0];
+        String B = input[1];
 
         int aLength = A.length();
         int bLength = B.length();
-        int first = 0 ;
-        int middle = 0;
-        int end = 0;
-
         if(aLength > bLength){
-            middle = bLength;
-            end = aLength;
+            for(int i = 0 ; i < aLength - bLength; i++ ){
+                int value = A.charAt(i);
+                sb.append(value -'0');
+            }
+            for(int i = 0; i < bLength; i++){
+                int add = (A.charAt(aLength - bLength + i) -'0') + (B.charAt(i) - '0');
+                sb.append(add);
+            }
         }
         else if(aLength < bLength){
-            
+
+            for(int i = 0; i < bLength - aLength; i++ ){
+                int value = B.charAt(i);
+                sb.append(value -'0');
+            }
+            for(int i = 0; i < aLength; i++){
+                int add = (A.charAt(i) -'0') + (B.charAt(bLength - aLength + i) - '0');
+                sb.append(add);
+            }
         }
+        else{
+            for(int i = 0 ; i < aLength; i++){
+                int add = (A.charAt(i) -'0') + (B.charAt(i) - '0');
+                sb.append(add);
+            }
+        }
+        System.out.println(sb.toString());
     }
 }
